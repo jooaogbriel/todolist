007690771c2ba1d7aa17d7d57da9b200db26bb55
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-11-12 10:46:51 -0300

    projeto-finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,37 @@ import { ListItem } from './components/ListItem';
 import { AddArea } from './components/AddArea'; 
 
 function App() {
-
-
   const [list, setList] = useState<Item[]>([
     {id: 1, name: 'Comprar pão', done: false},
     {id: 2, name: 'Ir na academia', done: true}
   ]);
 
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    })
+    setList(newList);
+  }
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-          <AddArea/>
+        <AddArea onEnter={handleAddTask} />
 
-         
+        {list.map((item, index)=>(
+          <ListItem
+            key={index}
+            item={item}
+          />
+        ))}
 
-         {list.map((item, index)=>(
-          <ListItem key={index} item={item}/>
-        ))} 
       </C.Area>
     </C.Container>
-  );
+);
 }
 
 export default App;
--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 import * as C from './styles'
 
-export const AddArea = () => {
+type Props = {
+    onEnter: (taskName: string) => void
+}
 
-    
+export const AddArea = ({onEnter}: Props) => {
+
+    const [inputText, setInputText] = useState('')
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code === 'Enter' && inputText !== ''){
+            onEnter(inputText)
+            setInputText('')
+        }
+    }
 
     return (
         <C.Container>
@@ -11,6 +22,9 @@ export const AddArea = () => {
             <input
             placeholder='Adicione uma tarefa'
             type='text'
+            value={inputText}
+            onChange={e=>setInputText(e.target.value)}
+            onKeyUp={handleKeyUp}
             />
         </C.Container>
     )
